e8dc46971347f62831559dc7d89de5869960b568
Noor <nonona319@gmail.com>
2022-12-04 11:27:23 -0900

    new

--- app/static/js/sidebar-dark.js
+++ app/static/js/sidebar-dark.js
@@ -1,30 +1,18 @@
 const body = document.querySelector('body'),
       sidebar = body.querySelector('nav'),
-      searchBtn = body.querySelector(".search-box"),
+      toggle = body.querySelector(".toggle"),
+      changeside = body.querySelector(".home"),
+      openside = body.querySelector(".menu-bar"),
       modeSwitch = body.querySelector(".toggle-switch"),
-      move = body.querySelector(".menu-bar"),
       modeText = body.querySelector(".mode-text");
-      let toggle = document.querySelector("#btn");
 
-      toggle.addEventListener("click" , () =>{
+
+toggle.addEventListener("click" , () =>{
     sidebar.classList.toggle("close");
 })
-
-searchBtn.addEventListener("click" , () =>{
+changeside.addEventListener("click" , () =>{
+    sidebar.classList.add("close");
+})
+openside.addEventListener("click" , () =>{
     sidebar.classList.remove("close");
 })
-move.addEventListener("click" , () =>{
-    sidebar.classList.remove("close");
-})
-modeSwitch.addEventListener("click" , () =>{
-    body.classList.toggle("dark");
-    
-    if(body.classList.contains("dark")){
-        modeText.innerText = "Light mode";
-    }else{
-        modeText.innerText = "Dark mode";
-        
-    }
-});
-
-
